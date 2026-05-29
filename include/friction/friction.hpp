e1b3faf6d7eaeb90e5c4a8ed927aaf8eefb8ae7a
Chase Hoffman <chase.hoffman@live.com>
2026-05-28 20:06:02 -0400

    project structure and build system improvements

---
+++ include/friction/friction.hpp
@@ -0,0 +1,46 @@
+#pragma once
+#include <memory>
+#include <cstdint>
+
+
+
+
+
+
+struct Snapshot {
+    uint64_t l1_cache_misses {0};
+    uint64_t l2_cache_misses {0};
+    uint64_t allocations {0};
+    uint64_t deletions {0};
+    uint64_t involuntary_context_switches {0};
+    uint64_t voluntary_context_switches {0};
+    uint64_t syscalls {0};
+};
+
+
+namespace friction {
+
+class Friction {
+    public: 
+
+    Friction();
+
+    ~Friction();
+
+
+    void start();
+
+    void stop();
+
+
+    private:
+
+    struct Impl;
+
+    std::unique_ptr<Impl> pimpl_;
+
+
+
+};
+
+} //namespace friction
